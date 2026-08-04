eeb95b1235afc51f293b6f5afde6fb4ffb88fe40
phai-zk <chisanupong.m34.1@gmail.com>
2026-08-04 07:22:49 +0700

    version 2 use Prisma console

--- backend_quest/src/db/db.ts
+++ backend_quest/src/db/db.ts
@@ -1,10 +1,13 @@
 import 'dotenv/config'
 import { PrismaClient } from './generated/client'
-import { PrismaNeon } from '@prisma/adapter-neon'
+import { PrismaPg } from '@prisma/adapter-pg';
+// import { PrismaNeon } from '@prisma/adapter-neon'
 
-const adapter = new PrismaNeon({
-  connectionString: process.env.DATABASE_URL!,
-})
+// const adapter = new PrismaNeon({
+//   connectionString: process.env.DATABASE_URL!,
+// })
+
+const adapter =new PrismaPg({ connectionString: process.env.DATABASE_URL })
 
 // export const prisma = new PrismaClient();
 export const prisma = new PrismaClient({ adapter })
