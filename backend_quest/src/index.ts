7ca5e1e026cd95ffda2b1656f67c791d0ac2b585
phai-zk <chisanupong.m34.1@gmail.com>
2026-08-04 22:55:55 +0700

    change way to use doc for vercel

--- backend_quest/src/index.ts
+++ backend_quest/src/index.ts
@@ -2,26 +2,23 @@ import { Hono } from "hono";
 import authRoutes from "./routes/auth.route.js";
 import itemRoutes from "./routes/lostItem.route.js";
 import userRoutes from "./routes/user.route.js";
-import { Scalar } from '@scalar/hono-api-reference'
-import fs from 'node:fs'
-import path from 'node:path'
-import yaml from 'yaml'
+import { Scalar } from '@scalar/hono-api-reference';
+import yaml from 'yaml';
+import path from "node:path";
+import fs from "node:fs";
+
+const filePath = path.join(process.cwd(), 'doc/openapi.yaml');
+const fileContent = fs.readFileSync(filePath, 'utf8');
+const parsedYaml = yaml.parse(fileContent);
 
 const app = new Hono();
 
 app.get("/", (c) => c.text("Hello, World!"));
 
 app.get('/doc', (c) => {
-  try {
-    const filePath = path.resolve('./doc/openapi.yaml')
-    const fileContent = fs.readFileSync(filePath, 'utf8')
-    const parsedYaml = yaml.parse(fileContent)
-    
-    return c.json(parsedYaml)
-  } catch (error) {
-    return c.text('Error reading OpenAPI spec', 500)
-  }
+  return c.json(parsedYaml)
 })
+
 app.get('/docs', Scalar({ url: '/doc' }))
 
 app.route("/api/auth", authRoutes);
