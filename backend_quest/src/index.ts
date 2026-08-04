f33df20b0ca126c0e846d80bf3d2445c93e26181
phai-zk <chisanupong.m34.1@gmail.com>
2026-08-05 00:04:03 +0700

    no read files

--- backend_quest/src/index.ts
+++ backend_quest/src/index.ts
@@ -3,23 +3,23 @@ import authRoutes from "./routes/auth.route.js";
 import itemRoutes from "./routes/lostItem.route.js";
 import userRoutes from "./routes/user.route.js";
 import { Scalar } from '@scalar/hono-api-reference';
-import yaml from 'yaml';
-import { join } from "node:path";
-import { readFileSync } from "node:fs";
+// import yaml from 'yaml';
+// import { join } from "node:path";
+// import { readFileSync } from "node:fs";
 
-const filePath = join(process.cwd(), 'doc/openapi.yaml');
-const fileContent = readFileSync(filePath, 'utf8');
-const parsedYaml = yaml.parse(fileContent);
+// const filePath = join(process.cwd(), 'doc/openapi.yaml');
+// const fileContent = readFileSync(filePath, 'utf8');
+// const parsedYaml = yaml.parse(fileContent);
 
 const app = new Hono();
 
 app.get("/", (c) => c.text("Hello, World!"));
 
-app.get('/doc', (c) => {
-  return c.json(parsedYaml)
-})
+// app.get('/doc', (c) => {
+//   return c.json(parsedYaml)
+// })
 
-app.get('/docs', Scalar({ url: '/doc' }))
+// app.get('/docs', Scalar({ url: '/doc' }))
 
 app.route("/api/auth", authRoutes);
 app.route("/api/items", itemRoutes);
